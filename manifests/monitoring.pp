65c7c083706ce50a33548cf41f9115431bfc1794
Zdenek Janda <zdenek.janda@cloudevelops.com>
2014-10-11 12:43:52 +0200

    fix add collectd

--- manifests/monitoring.pp
+++ manifests/monitoring.pp
@@ -1,6 +1,6 @@
 class mongodb_base::monitoring {
 
-  if $mongodb_base::base_monitoring_collectd {
+  if $mongodb_base::server::base_monitoring_collectd {
     include mongodb_base::monitoring::collectd
   }
 
