53fba34de92778251f7fdfd8866a3798ef6ef28c
Zdenek Janda <zdenek.janda@cloudevelops.com>
2014-10-11 12:05:31 +0200

    include tools and client

--- manifests/role/server.pp
+++ manifests/role/server.pp
@@ -1,5 +1,7 @@
 class mongodb_base::role::server {
 
   include mongodb_base::server
+  include mongodb_base::tools
+  include mongodb::client
 
 }
---
+++ manifests/tools.pp
@@ -0,0 +1,7 @@
+class mongodb_base::tools {
+
+  package {'mongodb-org-tools':
+    ensure => present
+  }
+
+}
