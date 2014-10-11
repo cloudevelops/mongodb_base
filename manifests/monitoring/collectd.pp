ed930bf8c9e5d77932f0bd3d7ed66e99c542370a
Zdenek Janda <zdenek.janda@cloudevelops.com>
2014-10-11 13:32:13 +0200

    added types

--- manifests/monitoring/collectd.pp
+++ manifests/monitoring/collectd.pp
@@ -16,4 +16,19 @@ class mongodb_base::monitoring::collectd {
     }
   }
 
+  concat::fragment { 'mongodb':
+    ensure => present,
+    target => '/etc/collectd/my_types.db',
+    content =>
+      "cache_ratio value:GAUGE:0:100\N
+      connections value:COUNTER:0:U\N
+      counter value:COUNTER:U:U\N
+      file_size bytes:GAUGE:0:U\N
+      memory value:GAUGE:0:281474976710656\N
+      percent percent:GAUGE:0:100.1\N
+      total_operations value:DERIVE:0:U\N",
+    order   => "10-${name}",
+  }
+
+
 }
